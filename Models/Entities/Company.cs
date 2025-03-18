99e3a08a3cd3d96f3d0f3928f648aa7c83887e4a
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-18 21:55:19 +0000

    Updated the ID in Company.cs

--- Models/Entities/Company.cs
+++ Models/Entities/Company.cs
@@ -5,7 +5,7 @@ namespace CompanyService.Entities
     public class Company
     {
         [Key]
-        public string Id { get; set; } 
+        public required string Id { get; set; } 
         public required string CompanyName { get; set; }
         public required string RegistrationNumber { get; set; }
         public required string Address { get; set; }
