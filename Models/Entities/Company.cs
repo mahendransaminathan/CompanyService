4b1e14b764a22ac505edd758710e8cf5bacf309a
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-18 22:52:29 +0000

    Updated to JSON string

--- Models/Entities/Company.cs
+++ Models/Entities/Company.cs
@@ -7,13 +7,26 @@ namespace CompanyService.Entities
     {
         [JsonProperty("id")] // Ensures the field is recognized as "id" in Cosmos DB
         public required string Id { get; set; } 
+        [JsonProperty("companyName")]
         public required string CompanyName { get; set; }
+
+        [JsonProperty("registrationNumber")]
         public required string RegistrationNumber { get; set; }
+
+        [JsonProperty("address")]
         public required string Address { get; set; }
+
+        [JsonProperty("city")]
         public required string City { get; set; }
+
+        [JsonProperty("state")]
         public required string State { get; set; }
+
+        [JsonProperty("country")]
         public required string Country { get; set; }
-        public required string Eircode { get; set; }     
+
+        [JsonProperty("eircode")]
+        public required string Eircode { get; set; }
         
     }
 }
